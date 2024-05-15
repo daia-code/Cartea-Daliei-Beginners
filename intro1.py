ec1634db4f70b04838e0a61f2f61db4379ef144f
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2024-05-15 10:39:22 +0300

    Create intro1.py

---
+++ intro1.py
@@ -0,0 +1,29 @@
+# Introducere in python
+# Variabilele se noteaza cu prima litera mica si pot aparea sub forma 
+# sub forma de cuvinte notate  numar, n ...
+# litere a,b,c ....
+
+# variabila = valoare (orice variabila are o valoare)
+numar = 10 
+rest = 0.5 
+cuvant = "Salut, tututor!"
+litera = 'a'
+
+# cum afisam mesaje in consola cu ajutorul print()
+print("Salut, tututor!") #pentru text
+print(numar) #pentru variabile, se va afisa valoare lui numar adica 10
+
+varsta = 20 #pentru a printa valoarea variabilei varsta trebuie inainte sa ii dati o valoare
+print(varsta)
+
+
+#operatii matematice
+print(numar + rest) # adunare
+print(numar -rest) #scadere
+print(numar * rest ) #inmultire
+print(numar / rest) #impartire
+print(numar % rest) #restul impartirii in cazul nostru 0
+print(numar // rest) #partea intreaga in urma impartirii
+
+#cum se compara doua numere pentru a vedea egalitatea?
+print(numar == rest) #egalitatea mereu se verifica  cu == in timp ce asignarea(atrbuirea) valorii se face cu =
