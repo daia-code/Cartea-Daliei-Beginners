b60add2e68e3b80cb6e3582f9e8bcef04a3565a1
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2024-05-22 09:43:13 +0300

    Create intro2.py

---
+++ intro2.py
@@ -0,0 +1,45 @@
+# comentariu singura linie
+
+'''Acesta este un comentariu
+  ce se poate scrie
+  pe mai multe linii '''
+
+print("Salut tuturor!")
+name="Daiana"
+print("Numele meu este ")
+print(name)
+
+
+print("Operatii matematice")
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+print(2//3)# div
+print(2%3) # modulo
+print(2**3)#ridicare la putere
+
+print(True==1 and 1==1)
+
+numar=1.0
+if(numar==1): 
+    #este afisat deoarece dupa virgula 1.0 nu are alta cifra, deci este intreg
+    #si sub aceasta forma
+    print("Numarul este egal cu 1")
+else:
+    #daca numarul nostru nu era egal cu 1, nu se respecta conditia si se
+    #executa acest cod
+    print("Numarul nu este egal cu 1")
+
+
+secv='c'
+if(secv=='a'):
+    print("am tastat a")
+elif(secv=='b'):
+    print("am tastat b")
+else:
+    print("este altceva")
+
+nume="Daiana"
+varsta=22
+print("Numele meu este "+nume+" si am "+str(varsta)+" de ani!")
