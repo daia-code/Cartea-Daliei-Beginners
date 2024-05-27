798c76c32ad0b7d39df8268ed9e383fb6ea9432d
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2024-05-27 22:19:58 +0300

    Create lectie.py

---
+++ s3/lectie.py
@@ -0,0 +1,50 @@
+# EXEMPLU COD : input()
+
+a = input()
+
+b = input("insert nr: ")
+
+
+# EXEMPLU COD : string
+
+a = "acesta este un sir de caractere"
+
+b = 'un alt sir de caractere'
+
+print(a)
+
+print(b)
+
+print("un sir de caractere dat ca parametru unei functii")
+
+# Ce va afișa următorul program și de ce.
+
+# Joacă-te cu input-ul și încearcă să explici care e diferența dintre = și ==
+
+
+a = input("insert a number")
+
+b = input("insert another number")
+
+print(a == b)
+
+# EXERCIȚIU : 2
+
+# Ce va afișa următorul program : 
+
+
+print(7 % 3)
+
+# EXERCIȚIU : 3
+
+# Ce va afișa următorul program : 
+
+
+print(7 // 3)
+
+# EXERCIȚIU : 4
+
+# Ce va afișa următorul program : 
+
+
+print(7 <= 3)
