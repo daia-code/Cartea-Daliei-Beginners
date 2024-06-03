d2a40185ef4ad6bfb80917bd00d146bac4cd7fa1
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2024-06-03 22:31:56 +0300

    Create exe1.py

---
+++ s4/exe1.py
@@ -0,0 +1,25 @@
+# liste de siruri de caractere :
+
+l = ["apple", "banana", "orange", "blueberries"]
+
+# liste de numere :
+
+l1 = [3, 4, 8, 1, 7, 2]
+
+# liste mixte :
+
+l2 = ["apple", 2, "banana", 8]
+
+# Cum accesam elementele unei liste :
+
+print(l[0], l[1], l[2])
+
+# Functia print poate primi mai mult de un parametru
+
+# Folosind bucla while
+
+while i < L:
+
+    print(l[i])
+
+    i = i + 1
