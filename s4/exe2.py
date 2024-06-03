1f3a5a47dd39904764f2a339fac217c9de4fe422
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2024-06-03 22:32:37 +0300

    Create exe2.py

---
+++ s4/exe2.py
@@ -0,0 +1,28 @@
+
+# Un exemplu cu o lista mai mare de elemente
+
+my_list = [23, "abc", 7, "def", 42, "ghi", 12, "jkl", 55, "mno", 18, "pqr", 34, "stu", 91,
+
+    "vwx", 2, "yzs", 8, "abc", 63, "def", 9, "ghi", 1, "jkl", 77, "mno", 29, "pqr", 46, 
+
+    "stu", 13, "vwx", 88, "yzs", 24, "abc", 71, "def",  39, "ghi", 5, "jkl", 68, "mno", 17, 
+
+    "pqr", 52, "stu", 96, "vwx", 31, "yzs", 57, "abc", 84, "def", 22, "ghi", 48, "jkl", 89, 
+
+    "mno", 35, "pqr", 70, "stu", 16, "vwx", 73, "yzs", 30, "abc", 62, "def", 11, "ghi", 95, 
+
+    "jkl", 28, "mno", 53, "pqr", 79, "stu", 43, "vwx", 67, "yzs", 14, "abc", 61, "def", 21, 
+
+    "ghi", 75, "jkl", 38, "mno", 83, "pqr", 44, "stu", 99, "vwx", 50, "yzs", 66, "abc", 25,
+
+    "def", 72, "ghi", 19, "jkl", 60, "mno", 87, "pqr", 36, "stu", 98, "vwx", 45, "yzs"]
+
+i = 0
+
+L = len(my_list)
+
+while i < L:
+
+    print(my_list[i])
+
+    i = i + 1
