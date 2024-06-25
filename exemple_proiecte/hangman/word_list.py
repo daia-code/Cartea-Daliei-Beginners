824c4b070edc1782e4fb733b174824b58aa189b3
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2024-06-25 08:40:34 +0300

    Create word_list.py

---
+++ exemple_proiecte/hangman/word_list.py
@@ -0,0 +1,8 @@
+words = [
+    "python", "development", "hangman", "programming", "algorithm",
+    "function", "variable", "condition", "loop", "syntax"
+]
+
+def get_random_word():
+    import random
+    return random.choice(words)
