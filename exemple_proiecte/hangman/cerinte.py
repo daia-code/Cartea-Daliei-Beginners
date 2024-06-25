7db1af0e616f59f71c002f180bb799e587cdad30
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2024-06-25 08:40:04 +0300

    Create cerinte.py

---
+++ exemple_proiecte/hangman/cerinte.py
@@ -0,0 +1,16 @@
+Jocul "Spânzurătoarea"
+
+Cerințe:
+Organizare: Folosiți module pentru a organiza codul în fișiere separate.
+  
+Funcționalități:
+Selectarea unui cuvânt de ghicit.
+Ghicirea literelor.
+Afișarea progresului.
+Gestionarea vieților jucătorului.
+Afișarea stării finale (câștig/pierdere).
+  
+Structura Proiectului:
+main.py: Scriptul principal care rulează jocul.
+word_list.py: Modul care conține lista de cuvinte.
+game_logic.py: Modul pentru logica jocului.
